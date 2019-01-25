b0f2f08c6e56418af7d1964f552eb8f79b07ad4f
Anh Duc <anhduc.hoang@vmodev.com>
2019-01-25 17:27:14 +0700

    add some form

---
+++ computedField.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <v-btn @click="compute">compute</v-btn>
+    {{result}}
+  </div>
+</template>
+
+<script>
+import jsonFn from 'json-fn';
+export default {
+    name: '',
+    props: ['model', 'field'],
+    data() {
+      return {
+        result: null  
+      }
+    },
+    inject: {
+      rootModel: {default: null},
+      path: {default: null},
+    },
+    methods: {
+      compute() {
+        const fn = jsonFn.clone(this.field.fn, true, true).bind(this.rootModel);
+        this.result = fn();
+      }
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
