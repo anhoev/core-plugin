f93ed1556549d0b36d71de6ec8d63d9868d0ccc6
Anh Duc <anhduc.hoang@vmodev.com>
2019-01-28 17:40:13 +0700

    add config filter

---
+++ dist/computedField.vue
@@ -0,0 +1,54 @@
+<template>
+
+  <div>
+    <v-btn @click="compute">compute</v-btn>
+    {{hello}}
+  </div>
+
+</template>
+    <script>
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _jsonFn = _interopRequireDefault(require("json-fn"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _default = {
+  name: '',
+  props: ['model', 'field'],
+
+  data() {
+    return {
+      result: null
+    };
+  },
+
+  inject: {
+    rootModel: {
+      default: null
+    },
+    path: {
+      default: null
+    }
+  },
+  methods: {
+    compute() {
+      const fn = _jsonFn.default.clone(this.field.fn, true, true).bind(this.rootModel);
+
+      this.result = fn();
+    }
+
+  }
+};
+exports.default = _default;
+</script> 
+<style scoped>
+
+
+
+</style>
