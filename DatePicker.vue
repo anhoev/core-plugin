fccd65c00d50f32659d6cb380eda1392abfacb78
LongNH <longnhse04527@fpt.edu.vn>
2019-04-12 18:51:49 +0700

    Date picker plugin

---
+++ DatePicker.vue
@@ -0,0 +1,153 @@
+<template>
+  <v-flex :class="flex" class="px-2">
+    <v-layout row>
+      <v-menu v-model="showMenu"
+              lazy
+              ref="menu"
+              :close-on-content-click="false"
+      >
+        <v-text-field v-model="computedModel"
+                      slot="activator"
+                      :label="field.label || field.key"
+                      prepend-icon="event"
+                      readonly
+        >
+        </v-text-field>
+        <v-card>
+          <v-card-text style="padding: 0 !important;">
+            <v-tabs centered grow v-model="showTab">
+              <v-tab key="datePicker">
+                <v-icon>event</v-icon>
+              </v-tab>
+              <v-tab key="timePicker" :disabled="!datePicked" v-if="field.pickerType === 'datetime'">
+                <v-icon>access_time</v-icon>
+              </v-tab>
+              <v-tabs-items>
+                <v-tab-item>
+                  <v-date-picker v-model="date"
+                                 :type="datePickerType"
+                                 scrollable
+                  >
+                  </v-date-picker>
+                </v-tab-item>
+                <v-tab-item :disabled="!datePicked" v-if="field.pickerType === 'datetime'">
+                  <v-time-picker v-model="time" :format="timePickerFormat" :use-seconds="field.pickSeconds"
+                  ></v-time-picker>
+                </v-tab-item>
+              </v-tabs-items>
+            </v-tabs>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn flat color="blue darken-1" @click="showMenu = false">Close</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-menu>
+    </v-layout>
+  </v-flex>
+</template>
+
+<script>
+
+  export default {
+    name: 'DatePicker',
+    props: ['model', 'field', 'noFlex'],
+    data() {
+      return {
+        menu: false,
+        showMenu: false,
+        selectedDateTime: null,
+        datePicked: false,
+        timePicked: false,
+        showTab: 0
+      };
+    },
+    created() {
+      if (this.model[this.field.key] ) {
+        this.selectedDateTime = this.model[this.field.key];
+        this.datePicked = true;
+      }
+    },
+    inject: {
+      rootModel: { default: null },
+      path: { default: null },
+      noLayout: { default: null }
+    },
+    computed: {
+      flex() {
+        return this.noLayout ? 'xs-12' : this.field.flex;
+      },
+      computedModel: {
+        get() {
+          this.computedModel = this.selectedDateTime;
+          if (this.field.pickerType === 'date') {
+            return this.selectedDateTime ? dayjs(this.selectedDateTime).format('DD/MM/YYYY') : '';
+          } else if (this.field.pickerType === 'month') {
+            return this.selectedDateTime ? dayjs(this.selectedDateTime).format('MM/YYYY') : '';
+          } else if (this.field.pickerType === 'datetime' && !this.field.is12Hour && !this.field.pickSeconds) {
+            return this.selectedDateTime ? dayjs(this.selectedDateTime).format('DD/MM/YYYY HH:mm') : '';
+          }  else if (this.field.pickerType === 'datetime' && this.field.is12Hour && this.field.pickSeconds) {
+            return this.selectedDateTime ? dayjs(this.selectedDateTime).format('DD/MM/YYYY hh:mm:ss A') : '';
+          } else if (this.field.pickerType === 'datetime' && this.field.pickSeconds && !this.field.is12Hour) {
+            return this.selectedDateTime ? dayjs(this.selectedDateTime).format('DD/MM/YYYY HH:mm:ss') : '';
+          } else if (this.field.pickerType === 'datetime' && this.field.is12Hour && !this.field.pickSeconds) {
+            return this.selectedDateTime ? dayjs(this.selectedDateTime).format('DD/MM/YYYY hh:mm A') : '';
+          } else  {
+            return this.selectedDateTime ? dayjs(this.selectedDateTime).format('YYYY-MM-DD[T]HH:mm') : '';
+          }
+        },
+        set(value) {
+          console.log(value);
+          this.$set(this.model, this.field.key, dayjs(value));
+        }
+      },
+      datePickerType() {
+        if (this.field) {
+          if (this.field.pickerType === 'month') {
+            return 'month';
+          }
+          return 'date';
+        }
+        return 'date';
+      },
+      date: {
+        get() {
+          return this.selectedDateTime ? dayjs(this.selectedDateTime).format('YYYY-MM-DD') : '';
+        },
+        set(value) {
+          const date = dayjs(value);
+          const year = date.year();
+          const month = date.month();
+          const dayOfMonth = date.date() ? date.date() : 1;
+          this.selectedDateTime = dayjs(new Date(year, month, dayOfMonth)).toDate();
+          this.datePicked = true;
+          this.showTab = 1;
+        }
+      },
+      time: {
+        get() {
+          return this.selectedDateTime ? dayjs(this.selectedDateTime).format('HH:mm') : '';
+        },
+        set(value) {
+          const [hour, minute, seconds] = value.split(':');
+          this.selectedDateTime = dayjs(this.selectedDateTime)
+            .set('hour', hour)
+            .set('minute', minute)
+            .set('seconds', seconds)
+            .toDate();
+        }
+      },
+      timePickerFormat() {
+        if (this.field.is12Hour) {
+          return 'ampm';
+        }
+        return '24hr';
+      }
+    },
+    methods: {
+    }
+  };
+</script>
+
+<style>
+</style>
