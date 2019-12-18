3278bd593d976c819872f9889244e1fa0fc5dea8
Anh <anhoev@yahoo.de>
2019-12-18 17:55:15 +0700

    fix ref-static

--- RefSelectStatic.vue
+++ RefSelectStatic.vue
@@ -1,6 +1,6 @@
 <template>
   <g-col :class="flex" class="px-2">
-    <g-combobox v-model="value" :items="options" :label="field.tableCell ? '': field.label || field.key" clearable item-value="text"
+    <g-combobox v-model="model[field.key]" :items="options" :label="field.tableCell ? '': field.label || field.key" clearable
                 @change="onChange"
                 :menu-props="{'z-index': 1000, 'closeOnContentClick': true}">
       <template #append-outer v-if="inArray">
@@ -34,19 +34,8 @@
       options() {
         const list = cms.Types[this.field.Type || this.field.ref].list;
         const labelProp = this.field.labelProp || cms.Types[this.field.Type || this.field.ref].info.title;
-        return list.map(item => ({text: item[labelProp], value: item}));
-      },
-      value: {
-        get() {
-          return {text: this.model[this.field.key] || ''};
-        },
-        set(v) {
-          this.model[this.field.key] = v ? v.text : undefined;
-        }
+        return list.map(item => item[labelProp]);
       }
-    },
-    mounted() {
-      this.value = {text: this.model[this.field.key]};
     },
     methods: {
       onChange(e) {
