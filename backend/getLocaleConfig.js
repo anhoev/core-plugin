f03f3de981fab1324f820c61dc105b7180834be4
Triracle <triracle@gmail.com>
2021-01-21 19:08:20 +0700

    Change post to on

--- backend/getLocaleConfig.js
+++ backend/getLocaleConfig.js
@@ -1,5 +1,5 @@
 module.exports = cms => {
-  cms.post('getTypes', async function (info) {
+  cms.on('getTypes', async function (info) {
     const localeConfig = await cms.getModel('SystemConfig').findOne({type: 'I18n'});
     if (localeConfig && localeConfig.content) {
       info.locale = localeConfig.content.locale;
